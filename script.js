b9fcbc12d7e433af13b61584c5a35422a5d08262
Vikash Gupta <cse.vikashgupta@gmail.com>
2020-12-05 20:19:38 +0530

    * Modifications for bookshelf

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function initBookShelf() {
   const panels = document.querySelectorAll('.book-shelf .panel')
-  panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
+  panels.forEach((panel, index) => {
+    panel.addEventListener('click', ($event) => {
       deactivateBooks(panels)
       panel.classList.add('active')
+      const position = index + 1
+      const image = getImageUrl(position)
+      panel.style.backgroundColor = 'transparent'
+      panel.style.backgroundImage = image
     })
   })
 }
 function deactivateBooks(panels) {
-  panels.forEach((panel) => {
+  panels.forEach((panel, index) => {
     panel.classList.remove('active')
+    panel.style.backgroundImage = 'none'
+    panel.style.backgroundColor = setBackgroundColor(index)
   })
+}
+function getImageUrl(position) {
+  return 'url(./img/' + position + '.jpg)'
+}
+function setBackgroundColor(position) {
+  const colors = ['#73c2fb', '#f4c430', '#d8dcd6', '#36454f', '#40e0d0']
+  return colors[position]
 }
 initBookShelf()
